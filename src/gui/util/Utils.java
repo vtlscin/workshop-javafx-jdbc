8aadc572935579968b0e1d1417e6a9207e7772db
Vinicius Thiago <vtls@cin.ufpe.br>
2020-03-25 10:59:21 -0300

    Saving a new Department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -11,4 +11,16 @@ public class Utils {
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
 	}
 	
+	public static Integer tryParseToInt(String str)
+	{
+		try {
+			
+			return Integer.parseInt(str);
+			
+		} catch (NumberFormatException e) {
+			
+			return null;
+		}
+		
+	}
 }

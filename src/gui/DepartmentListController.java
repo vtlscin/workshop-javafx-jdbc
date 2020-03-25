73689874cb5c2065330dd750a90723dc6d354143
Vinicius Thiago <vtls@cin.ufpe.br>
2020-03-25 10:19:53 -0300

    DepartmentForm design

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
@@ -36,9 +45,10 @@ public class DepartmentListController implements Initializable {
 	private ObservableList<Department> obsList;
 	
 	@FXML
-	public void onBtNewAction()
+	public void onBtNewAction(ActionEvent event)
 	{
-		System.out.println("Button Clicked !!!");
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm(parentStage, "/gui/DepartmentForm.fxml");
 	}
 	
 	public void setDepartmentService (DepartmentService service)
@@ -59,6 +69,10 @@ public class DepartmentListController implements Initializable {
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
 	}
 
+	
+	/**
+	 * Metodo responsavel por exibir informaçoes do banco na tela de departamento
+	 */
 	public void updateTableView()
 	{
 		if (service == null)
@@ -69,6 +83,29 @@ public class DepartmentListController implements Initializable {
 		List<Department> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartment.setItems(obsList);
+	}
+	
+	/**
+	 * @param metodo para carregar a janela de inserçao de departamentos
+	 * @param absoluteName
+	 */
+	private void createDialogForm(Stage parentStage, String absoluteName) {
+		try {
+			
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter Department data");
+			dialogStage.setScene(new Scene(pane)); // elemento raiz da cena
+			dialogStage.setResizable(false); // indica que a janela pode ser redimensionada 
+			dialogStage.initOwner(parentStage); // indica o stage pai da janela
+			dialogStage.initModality(Modality.WINDOW_MODAL); // indica se a janela é modal
+			dialogStage.showAndWait();
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 	
 }

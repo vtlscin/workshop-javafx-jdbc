7142e78df5b1e2ec32c449403c202b8baa9c6cc3
Vinicius Thiago <vtls@cin.ufpe.br>
2020-03-31 15:21:57 -0300

    Saving Seller

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -32,6 +32,18 @@ public class Utils {
 
 	}
 
+	public static Double tryParseToDouble(String str) {
+		try {
+
+			return Double.parseDouble(str);
+
+		} catch (NumberFormatException e) {
+
+			return null;
+		}
+
+	}
+	
 	public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) {
 		tableColumn.setCellFactory(column -> {
 			TableCell<T, Date> cell = new TableCell<T, Date>() {
